b418d7c1109bd224e179dda61dd3459f24787cfe
Bernd Herding <bernd.herding@external.biontech.com>
2025-06-24 23:15:46 +0200

    fix (or skip) checkov issuesfg

--- terraform-deployment-role.tf
+++ terraform-deployment-role.tf
@@ -4,6 +4,7 @@ resource "aws_iam_role" "terraform_deployment_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "terraform_deployment_role" {
+  # checkov:skip=CKV_AWS_274: AWS AdministratorAccess policy is used by IAM roles, users, or groups
   role       = aws_iam_role.terraform_deployment_role.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
