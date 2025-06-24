f95fd83012e85eb5cf1918942f0780513a5c2669
Bernd Herding <bernd.herding@external.biontech.com>
2025-06-25 00:05:37 +0200

    add terraform validate check

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,9 @@
 terraform {
-  backend "s3" {}
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 6"
     }
   }
-  required_version = ">= 1.12"
+  required_version = ">= 1.11.4"
 }
