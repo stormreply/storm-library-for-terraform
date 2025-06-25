ef4be0f917c57f1b1f522e57db6bdc572177ffd6
Bernd Herding <bernd.herding@external.biontech.com>
2025-06-25 10:56:06 +0200

    modify comments slightly

--- terraform-deployment-role.tf
+++ terraform-deployment-role.tf
@@ -3,8 +3,9 @@ resource "aws_iam_role" "terraform_deployment_role" {
   assume_role_policy = data.aws_iam_policy_document.github_oidc_trust_policy.json
 }
 
+# TODO:
 resource "aws_iam_role_policy_attachment" "terraform_deployment_role" {
-  # checkov:skip=CKV_AWS_274: AWS AdministratorAccess policy is used by IAM roles, users, or groups
+  # checkov:skip=CKV_AWS_274: "AWS AdministratorAccess policy is used by IAM roles, users, or groups"
   role       = aws_iam_role.terraform_deployment_role.name
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
