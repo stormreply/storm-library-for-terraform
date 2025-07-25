ddbe0dba6429d1648546de5c167fff9435289cfe
Bernd Herding <bernd.herding@external.biontech.com>
2025-07-25 10:39:41 +0200

    add github_principal to oidc wf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "backend_bucket" {
   description = "Central backend bucket of the Storm Library for Terraform (SLT)™"
   type        = string
 }
+
+variable "github_principal" {
+  description = "Github owner (org or user) of repositories permitted to deploy to AWS"
+  type        = string
+}
