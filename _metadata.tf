3423b248aecd080eec8850959b79bfb11c7aa32a
Bernd Herding <bernd.herding@external.biontech.com>
2025-12-23 19:23:41 +0100

    remove environment from metadata

--- _metadata.tf
+++ _metadata.tf
@@ -12,30 +12,28 @@ locals {
 
 variable "_metadata" {
   type = object({
-    actor       = string # Github actor (deployer) of the deployment
-    catalog_id  = string # SLT catalog id of this module
-    environment = string # environment of the deployment, defaults to <actor>
-    deployment  = string # slt-<catalod_id>-<repo>-<environment>
-    ref         = string # Git reference of the deployment
-    ref_name    = string # Git ref_name (branch) of the deployment
-    repo        = string # GitHub short repository name (without owner) of the deployment
-    repository  = string # GitHub full repository name (including owner) of the deployment
-    sha         = string # Git (full-length, 40 char) commit SHA of the deployment
-    short_name  = string # slt-<cat>-<environment>
-    time        = string # Timestamp of the deployment
+    actor      = string # Github actor (deployer) of the deployment
+    catalog_id = string # SLT catalog id of this module
+    deployment = string # slt-<catalod_id>-<repo>-<actor>
+    ref        = string # Git reference of the deployment
+    ref_name   = string # Git ref_name (branch) of the deployment
+    repo       = string # GitHub short repository name (without owner) of the deployment
+    repository = string # GitHub full repository name (including owner) of the deployment
+    sha        = string # Git (full-length, 40 char) commit SHA of the deployment
+    short_name = string # slt-<catalog_id>-<actor>
+    time       = string # Timestamp of the deployment
   })
   default = {
-    actor       = ""
-    catalog_id  = ""
-    environment = ""
-    deployment  = ""
-    ref         = ""
-    ref_name    = ""
-    repo        = ""
-    repository  = ""
-    sha         = ""
-    short_name  = ""
-    time        = ""
+    actor      = ""
+    catalog_id = ""
+    deployment = ""
+    ref        = ""
+    ref_name   = ""
+    repo       = ""
+    repository = ""
+    sha        = ""
+    short_name = ""
+    time       = ""
   }
 }
 
